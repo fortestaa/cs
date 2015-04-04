2877214c07b06bed59fe754b211b13f0842fe1f5
fortestaa <for_relaxaa@163.com>
2015-04-04 14:44:21 -0700

    Create Implement_strStr.java

---
+++ Implement_strStr.java
@@ -0,0 +1,18 @@
+package easy;
+
+public class Implement_strStr {
+	public int strStr(String haystack, String needle) {
+		for (int i = 0; ; i++) {
+			for (int j = 0; ; j++) {
+				if (j == needle.length()) {
+					return i;
+				} 
+				if (i + j == haystack.length()) {
+					return -1;
+				}
+				if (haystack.charAt(i + j) != needle.charAt(j))
+					break;
+			}
+		}
+	}
+}
