1aceeb9c93d07b9f227db1e8082f65efa63f9aaf
fortestaa <for_relaxaa@163.com>
2015-04-04 19:50:57 -0700

    Create Graph.java

---
+++ Graph.java
@@ -0,0 +1,11 @@
+package chapter4;
+
+import java.util.LinkedList;
+
+public class Graph {
+	private static LinkedList<Node> nodes;
+	
+	public static LinkedList<Node> getNodes() {
+		return nodes;
+	}
+}
