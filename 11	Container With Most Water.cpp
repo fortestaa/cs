35a06c65692af05f4eb9c9a1c666a43536cb6440
fortestaa <for_relaxaa@163.com>
2015-04-09 20:51:34 -0700

    Create 11	Container With Most Water.cpp

---
+++ 11	Container With Most Water.cpp
@@ -0,0 +1,26 @@
+//
+//  11	Container With Most Water.cpp
+//
+
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int> &height) {
+        int start = 0;
+        int end = height.size() - 1;
+        int maxV = INT_MIN;
+        while (start < end) {
+            int contain = min(height[end], height[start]) * (end - start);
+            maxV = max(maxV, contain);
+            if (height[start] <= height[end]) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+        return maxV;
+    }
+};
